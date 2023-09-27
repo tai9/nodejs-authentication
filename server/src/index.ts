13f2502c27abde49ae7b6d4c7bf4c969e9957329
Tai <tantai.it95@gmail.com>
2023-09-27 22:22:29 +0700

    feat: allow cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const port = 4100;
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("ok");
+  res.status(200).json({
+    status: "ok",
+  });
 });
 
 app.listen(port, () => {
