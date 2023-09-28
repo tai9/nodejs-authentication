9b5b7e06807e218027f766a2171edd251572f37c
Tai <tantai.it95@gmail.com>
2023-09-28 21:43:05 +0700

    chore: update postgres env

--- server/src/db.ts
+++ server/src/db.ts
@@ -8,11 +8,11 @@ export const getDbConnection = async () => {
 
   AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.POSTGRES_HOST,
+    port: +process.env.POSTGRES_PORT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
     entities: [User],
     synchronize: process.env.NODE_ENV === "development",
     logging: false,
