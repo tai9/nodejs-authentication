4cfe725746724ac7aeae1bdf9a0e28a396993e5e
Tai <tantai.it95@gmail.com>
2023-09-29 15:40:27 +0700

    debug: deploying

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,13 @@ import "reflect-metadata";
 import { getDbConnection } from "./db";
 import app from "./app";
 
-(async () => {
-  await getDbConnection();
+getDbConnection();
 
-  return app.listen(app.get("port"), () => {
-    console.log(
-      "  App is running at http://localhost:%d in %s mode",
-      app.get("port"),
-      app.get("env")
-    );
-    console.log("  Press CTRL-C to stop\n");
-  });
-})();
+app.listen(app.get("port"), () => {
+  console.log(
+    "  App is running at http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env")
+  );
+  console.log("  Press CTRL-C to stop\n");
+});
