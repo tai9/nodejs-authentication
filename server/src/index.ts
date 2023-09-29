1c8eaf13ac965881209e612ff2792e2896082f36
Tai <tantai.it95@gmail.com>
2023-09-29 15:14:15 +0700

    debug: deploying

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,13 +5,14 @@ import app from "./app";
 
 async function init() {
   // await getDbConnection();
-  // return app.listen(app.get("port"), () => {
-  //   console.log(
-  //     `App is running at http://localhost:%d in %s mode`,
-  //     app.get("port"),
-  //     app.get("env")
-  //   );
-  // });
+
+  return app.listen(4001, () => {
+    console.log(
+      `App is running at http://localhost:%d in %s mode`,
+      app.get("port"),
+      app.get("env")
+    );
+  });
 }
 
 const server = init();
