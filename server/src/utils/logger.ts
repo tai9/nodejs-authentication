081674a7cc0ccedc4ebcb542ad5abb378eb94357
Tai <tantai.it95@gmail.com>
2023-09-29 16:35:40 +0700

    chore: disable write log file

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -3,7 +3,7 @@ import "winston-daily-rotate-file";
 import { format } from "logform";
 
 const isProd = process.env.NODE_ENV === "production";
-const appName = process.env.APP_NAME || "app";
+// const appName = process.env.APP_NAME || "app";
 const logLevel = process.env.LOG_LEVEL || "info";
 
 const options: winston.LoggerOptions = {
@@ -14,20 +14,20 @@ const options: winston.LoggerOptions = {
   ],
 };
 
-if (process.env.NODE_ENV != "test") {
-  // @ts-ignore
-  options.transports.push(
-    // @ts-ignore
-    new winston.transports.DailyRotateFile({
-      level: logLevel,
-      filename: `logs/${appName}-%DATE%.log`,
-      datePattern: "YYYY-MM-DD-HH",
-      zippedArchive: true,
-      maxSize: "20m",
-      maxFiles: "30d",
-    })
-  );
-}
+// if (process.env.NODE_ENV != "test") {
+//   // @ts-ignore
+//   options.transports.push(
+//     // @ts-ignore
+//     new winston.transports.DailyRotateFile({
+//       level: logLevel,
+//       filename: `logs/${appName}-%DATE%.log`,
+//       datePattern: "YYYY-MM-DD-HH",
+//       zippedArchive: true,
+//       maxSize: "20m",
+//       maxFiles: "30d",
+//     })
+//   );
+// }
 
 if (!isProd) {
   options.format = format.simple();
